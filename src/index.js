3b92191a79aa10352c256b104fb5570a058d2306
Nikita K <rabot422@gmail.com>
2023-11-20 23:57:09 +0300

    add change active circle after change photo

--- src/index.js
+++ src/index.js
@@ -23,12 +23,20 @@ function nextImage(){
     console.log(activeImage,activeImage.previousElementSibling)
     activeImage.nextElementSibling.classList.add('active_item')
     activeImage.classList.remove('active_item')
+
+    const activeCircle = document.querySelector('.active_circle')
+    activeCircle.nextElementSibling.classList.add('active_circle')
+    activeCircle.classList.remove('active_circle')
 }
 function previousImage(){
     const activeImage = document.querySelector('.active_item')
     console.log(activeImage,activeImage.previousElementSibling)
     activeImage.previousElementSibling.classList.add('active_item')
     activeImage.classList.remove('active_item')
+
+    const activeCircle = document.querySelector('.active_circle')
+    activeCircle.previousElementSibling.classList.add('active_circle')
+    activeCircle.classList.remove('active_circle')
 }
 
 leftBtn.addEventListener('click', ()=>{
